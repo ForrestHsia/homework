358e9bff362738584d2497015b244067b86f7a2d
pkh-forresthsia <pkh_forresthsia@photons.com.tw>
2022-08-05 17:53:33 +0800

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -1,4 +1,12 @@
 // 轉接器模式
+// 模式使用狀況條件
+// 教練是葉基八，然後有個 DOM 球員赫基八
+// 業績八可以 "直接" 操縱 林智勝，讓他打安打或者守備，但沒辦法 "直接" 操縱 赫基八
+// 業績八不懂洋文 (換成物件導向的狀況，就是 物件A 無法調用 物件B，可是 物件A 又需要 物件B 的某function 或者something)
+// 這時，可以設一個腳色叫做翻譯，也就是謝基八
+// 業績八 -直接操縱-> 謝基八 -直接操縱-> 赫基八
+//   |                                  ↑
+//   ↓______________間接操縱_____________|
 
 class Player{
     constructor(name){
@@ -51,6 +59,7 @@ class Translator extends Player{
         let DOMfielder1 = null; 
         DOMfielder1 = new DOMFielder(name);
         this.player = DOMfielder1
+        
     }
 
     hit(){
@@ -61,8 +70,10 @@ class Translator extends Player{
 // 用戶端
 wang40 = new TWNPitcher("王建民");
 zho16 = new TWNFielder("周思齊");
-translator = new Translator("赫雷拉")
+// helayla = new DOMFielder("赫基八")
+translator = new Translator("赫基八")
 
 wang40.pitch();
 zho16.hit();
+// helayla.hitDom();
 translator.hit();
